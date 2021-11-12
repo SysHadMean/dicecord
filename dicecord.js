1d095dcc1374abff12e2fdcd06bd01dab205476e
SysHadMean <17857978+SysHadMean@users.noreply.github.com>
2021-11-12 03:02:49 +0100

    add better-logging

--- dicecord.js
+++ dicecord.js
@@ -8,6 +8,7 @@ const Include = require("./include/resource.priv.json");
 //const client = new Discord.Client();
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+require('better-logging')(console);
 
 const reSpec = /\W|_/g;
 const prefix = Include.prefix;
